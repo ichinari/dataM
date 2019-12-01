c4446b7160a957d3265909084e353281558b8ef1
Kazuya <>
2019-12-01 16:13:01 +0900

    add new file

---
+++ Data_Human/js/app.js
@@ -0,0 +1,12 @@
+$(function(){
+    //smartphone-menu-tab
+    $('.js-toggle-sp-menu').on('click', function(){
+        $(this).toggleClass('active');
+        $('.js-toggle-sp-menu-target').toggleClass('active');
+    });
+    
+    $('.js-menu-close').on('click',function(){
+        
+    });
+    
+});
